7d21783ef3990432bba7ee0cc855ae864e0ebc2f
Sparsh Kesari <sparshk@lt-mba-197.bbrouter>
2022-09-09 16:15:32 +0530

    project init

---
+++ data/siteMetadata.js
@@ -0,0 +1,31 @@
+const siteMetadata = {
+  title: 'Braydon Coyer – full-stack engineer, writer, creative developer.',
+  author: 'Braydon Coyer',
+  headerTitle: "Manoj's Portfolio",
+  description:
+    "I'm Braydon. I'm a senior front end developer, blogger and designer. Welcome to my corner of the internet. I'm glad you're here!",
+  language: 'en-us',
+  siteUrl: '',
+  siteRepo: 'https://github.com/manoj9788',
+  avatarImage:
+    'https://cdn.discordapp.com/attachments/761280961605664780/1006623583382020217/unknown.png',
+  socialBanner:
+    'https://res.cloudinary.com/braydoncoyer/image/upload/v1638884997/default-social-image.png',
+  email: 'mailto:braydon@coyer.net',
+  github: 'https://github.com/braydoncoyer',
+  twitter: 'https://twitter.com/BraydonCoyer',
+  twitterHandle: '@BraydonCoyer',
+  codepen: 'https://codepen.io/braydoncoyer',
+  linkedin: 'https://www.linkedin.com/in/braydon-coyer/',
+  newsletter: 'https://www.getrevue.co/profile/braydoncoyer',
+  snippets:
+    'https://www.notion.so/9f35162787e0448094188b66304cf9f6?v=1b9c4e146edf46148fe7b1c5346cbea4',
+  carbonCode: 'CESDE27N',
+  carbonPlacement: 'braydoncoyerdev',
+  locale: 'en-US',
+  analytics: {
+    plausibleDataDomain: 'braydoncoyer.dev'
+  }
+};
+
+module.exports = siteMetadata;
